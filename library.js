10250313cbdd9e0836354225d08b17703798a542
cyber-jay <54187020+cyber-jay@users.noreply.github.com>
2021-06-18 16:08:49 -0600

    Create library.js

---
+++ library.js
@@ -0,0 +1,13 @@
+// Random Library
+
+// Add a random Grade
+function randNum(low, high) {
+   return Math.random() * (high - low) + low;
+}
+
+// Random Grade to 100;
+
+function randomInt(low, high) {
+   return Math.floor(Math.random() * (high - low) + low);
+}
+
